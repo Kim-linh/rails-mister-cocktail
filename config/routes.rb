d4d9d92a2f4aec4b3b2d786cbd75058474353eb0
maeva pham <maevapham.contact@gmail.com>
2019-05-16 15:48:14 +0200

    creation controller cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+  get 'cocktails/new'
+  get 'cocktails/create'
+  get 'cocktails/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails
 end
